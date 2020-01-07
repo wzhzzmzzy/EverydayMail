ffe0757c38cfc78e5cf0de3078eb48c027f26399
wzhzzmzzy <wzhzzmzzy@foxmail.com>
2020-01-07 13:15:02 +0800

    Update README

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# EverydayMail
-Everyday Good Morning E-mail
+# 每日早安邮件 EverydayMail@TypeScript
+
+闲的无聊，整了个每日早安邮件定时脚本。使用它，可以每天定时发送早安邮件哦。
+
+出于熟悉 TS 的目的，基本使用 TypeScript 编写。总入口是`main,js`，实际入口是`src/index.ts`。
+
+## 使用方式
+
+1. 准备
+
+```shell
+git clone https://github.com/wzhzzmzzy/EverydayMail.git
+yarn
+```
+
+2. 修改`config.yaml`，具体参看文件注释
+
+```shell
+yarn dev
+```
+
+## 主要功能
+
+- 定时发送邮件：
+  - 定时功能使用`js-schedule`；
+  - 邮件功能使用`nodemailer`；
+  - 邮件模板使用`pug`语法；
+- 爬取 ONE·一个 [每日更新](http://wufazhuce.com/)。
+- 和风天气[每日预报](https://www.heweather.com/)。
--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,14 @@ import {
     renderTemplate, sendMail, loadOptions, Options
 } from './utils';
 
-async function run(options: Options) {
+async function run() {
+    console.log('Loading config.yaml...');
+    const options = loadOptions('config.yaml');
     console.log('Start task, now is ' + (new Date()));
     try {
         const one = await oneDaily();
         const weather = await weatherReport(options.HeWeatherKey);
-        const mailContent = renderTemplate('src/mail.pug', one, weather);
+        const mailContent = renderTemplate('templates/mail.pug', one, weather);
         sendMail(mailContent, options.SMTP, options.Mails);
     } catch(err) {
         console.log(err);
@@ -20,7 +22,7 @@ async function run(options: Options) {
 }
 
 export async function main() {
-    run(loadOptions('config.yaml'));
-    // console.log('Scheduling...')
-    // scheduleJob('0 30 7 * * *', run);
+    // run();
+    console.log('Scheduling...')
+    scheduleJob('0 30 7 * * *', run);
 }
